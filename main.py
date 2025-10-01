94068e0b78a8308ea618105b0a31b89b01fe788b
Hazel Sibhuku <hasibjhb025>
2025-10-01 15:40:49 +0200

    expanded tests

--- main.py
+++ main.py
@@ -1,56 +1,59 @@
-def factorial(n):
-    fact = 1
-    for i in range(1, n+1):
-        if n < 0:
-            return "input positive number"
-        if n == 0:
-            return 1
-        else:
-            fact *= i
-    return fact
+# def factorial(n):
+#     fact = 1
+#     for i in range(1, n+1):
+#         if n < 0:
+#             return "input positive number"
+#         if n == 0:
+#             return 1
+#         else:
+#             fact *= i
+#     return fact
 
-# print(factorial(3))
+# # print(factorial(3))
 
 def fibonacci(n):
     """
     fn = f(n-1)+ f(n-2)
     """
+    if not n.isdigit():
+        raise ValueError or NameError
     if n < 0:
         return "only positive integers"
-
-
-    seq = [0,1]
-    for i in range(2,n+1):
-        num = seq[i-1] + seq[i-2]
-        seq.append(num)
-        nth = seq[-1]
+    elif n == 1:
+        return "only numbers above 1"
+    else:
+        seq = [0,1]
+        for i in range(2,n+1):
+            num = seq[i-1] + seq[i-2]
+            seq.append(num)
+            nth = seq[-1]
 
     return nth, seq
 
-# print(fibonacci(6))
+print(fibonacci("p"))
     
 
-def fizzbuzz(n):
-    result = []
-    for i in range(1, n+1):  
-    # while n <= n:  
-        if i%3==0 and i%5==0:
-            result.append("FizzBuzz")
+# def fizzbuzz(n):
+#     result = []
+#     for i in range(1, n+1):  
+#     # while n <= n:  
+#         if i%3==0 and i%5==0:
+#             result.append("FizzBuzz")
 
-        elif i%3==0:
-           result.append("Fizz")
+#         elif i%3==0:
+#            result.append("Fizz")
 
-        elif i%5==0:
-            result.append("Buzz")
+#         elif i%5==0:
+#             result.append("Buzz")
             
-        else:
-            result.append(i) 
+#         else:
+#             result.append(i) 
 
-    return result
+#     return result
 
-    # return i
-# fizzbuzz(3)
-# print(fizzbuzz(3))
+#     # return i
+# # fizzbuzz(3)
+# # print(fizzbuzz(15))
 
 
 
--- test_main.py
+++ test_main.py
@@ -2,15 +2,19 @@ import unittest
 from main import factorial, fibonacci, fizzbuzz
 
 class TestMain(unittest.TestCase):
-    def test_main(self):
+    def test_fizzbuzz(self):
         self.assertEqual(fizzbuzz(3),[1,2, 'Fizz'])
+        self.assertEqual(fizzbuzz(5),[1, 2, 'Fizz', 4, 'Buzz'])
+        self.assertEqual(fizzbuzz(15),[1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz'])
+        
 
+
+    def test_factorial(self):
         self.assertEqual(factorial(3), 6)
 
+
+    def test_fibonacci(self):
         self.assertEqual(fibonacci(6),(8, [0,1,1,2,3,5,8]))
-
-
-
 
 
 if __name__ == "__main__":
